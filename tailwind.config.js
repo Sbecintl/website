35ceea6c4a56008d7e53322e4e9a48f87bc143ef
Funsho <funshoajayi29@gmail.com>
2024-05-13 04:26:29 +0100

    added footer, adding imge to hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,40 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          red: "#ffffff",
+          offRed: "#FD0000ff",
+          black: "#000000ff",
+          gray: "#505050ff",
+          white: "#FFFFFFff",
+          night: "#0C0C0Cff",
+          platinum: "#E3E3E3ff",
+        },
+      },
+      animation: {
+        dropBounce: "dropBounce 1s ease-in-out infinite",
+        slideIn: "slideIn 5s ease-in-out infinite",
+        fadeIn: "fadeIn 2s ease-in-out ",
+      },
+      keyframes: {
+        drop: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+
+        dropBounce: {
+          "0%": { top: -"300px" },
+          "40%": { top: "0px" },
+          "70%": { top: -"5px" },
+          "100%": { top: "0px" },
+        },
+
+        slideIn: {
+          "0%": { left: -"200px" },
+          "100%": { left: "0px" },
+        },
+
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
       },
     },
